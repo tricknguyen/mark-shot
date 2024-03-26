d4972133bf80f56a80c1b949045c89d6cbcd5c68
anhvunguyen-preciofishbone <vu.nguyen.anh@preciofishbone.se>
2024-03-26 16:27:17 +0700

    Refactoring code and update a bit UI 🎠

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,76 +1,92 @@
 import { colors } from "@/models/GradientLibrary";
-import { Card } from "./ui/card";
 import { Slider } from "./ui/slider";
 import { useAtom } from "jotai";
 import { settingAttom } from "@/store/SettingStore";
 import { ColorGradient } from "@/models/ColorGradient";
 import { Button } from "./ui/button";
+import { Plus } from 'lucide-react';
 
 export function Settings() {
     const [settings, setSettings] = useAtom(settingAttom);
 
     function onSelectColor(color: ColorGradient) {
-        setSettings({ ...settings, backgroundColor: {
-            title: color.from,
-            value: `linear-gradient(90deg, #${color.from} 0%, #${color.to} 100%)`
-        }})
+        setSettings({
+            ...settings, backgroundColor: {
+                title: color.from,
+                value: `linear-gradient(90deg, #${color.from} 0%, #${color.to} 100%)`
+            }
+        })
     }
 
     function onSelectPadding(val: number) {
-        setSettings({ ...settings, padding: val});
+        setSettings({ ...settings, padding: val });
     }
 
     function onSelectCorner(val: number) {
-        setSettings({ ...settings, corner: val});
+        setSettings({ ...settings, corner: val });
     }
 
     function onSelectShadow(val: number) {
-        setSettings({ ...settings, shadow: val});
+        setSettings({ ...settings, shadow: val });
+    }
+
+    function renderGradientSetting() {
+        return <div className="py-2 border-b">
+            <h3 className="font-semibold leading-none tracking-tight">
+                Gradient
+            </h3>
+            <div className="grid grid-cols-4 pt-2">
+                {
+                    colors.map((color, key) => {
+                        return <div key={key} className="flex justify-center">
+                            <Button
+                                className="w-[50px] h-[50px] mb-2"
+                                style={{ backgroundImage: `linear-gradient(90deg, #${color.from} 0%, #${color.to} 100%)` }}
+                                onClick={() => { onSelectColor(color); }}
+                            />
+                        </div>
+                    })
+                }
+                <div className="flex justify-center">
+                    <Button
+                        className="w-[50px] h-[50px] mb-2"
+                        variant="outline"
+                        size="icon"
+                    >
+                        <Plus />
+                    </Button>
+                </div>
+            </div>
+        </div>
     }
 
     return (
         <div className="py-4 px-2 lg:block">
-            
-            <Button className="w-full">None</Button>
-
-            <div className="py-2 border-b">
-                <h3 className="font-semibold leading-none tracking-tight">
-                    Gradient
-                </h3>
-                <div className="grid grid-cols-3 pt-2">
-                    {
-                        colors.map((color, index) => {
-                            return <div key={index} className="flex justify-center"> 
-                                <Card 
-                                    className="w-[80px] h-[80px] mb-2" 
-                                    style={{backgroundImage: `linear-gradient(90deg, #${color.from} 0%, #${color.to} 100%)`  }} 
-                                    onClick={() => {onSelectColor(color);}}
-                                />
-                            </div> 
-                        })
-                    }
-                </div>
+            <div className="pb-2 border-b">
+                <Button className="w-full">None</Button>
             </div>
+
+            {renderGradientSetting()}
 
             <div className="px-3 py-6 border-b ">
                 <h2 className="text-lg font-semibold mb-2">Padding</h2>
                 <Slider defaultValue={[50]} max={300} step={1} onValueChange={(val) => {
                     onSelectPadding(val[0]);
-                }}/>
+                }} />
             </div>
 
             <div className="px-3 py-6 border-b ">
                 <h2 className="text-lg font-semibold mb-2">Corner</h2>
                 <Slider defaultValue={[10]} max={100} step={1} onValueChange={(val) => {
                     onSelectCorner(val[0]);
-                }}/>
+                }} />
             </div>
 
             <div className="px-3 py-6">
                 <h2 className="text-lg font-semibold mb-2">Shadow</h2>
                 <Slider defaultValue={[10]} max={100} step={1} onValueChange={(val) => {
                     onSelectShadow(val[0]);
-                }}/>
+                }} />
             </div>
         </div>
     );
