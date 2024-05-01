ac7c55ee33c9072758faeaf8c938cb44ffd007a9
anhvunguyen-preciofishbone <vu.nguyen.anh@preciofishbone.se>
2024-05-01 14:58:43 +0700

    Update Utils convert color 👝

--- src/utils/ConvertColor.ts
+++ src/utils/ConvertColor.ts
@@ -1,20 +1,22 @@
 export module Utils {
     export function generateGradient(colors: string[]): string {
-        const numColors = colors.length;
-        if (numColors === 0) {
-            return ''; // No colors provided
+        const angle = 45; 
+
+        if (colors.length === 1) {
+            return `linear-gradient(${angle}deg, ${colors[0]} 0%, ${colors[0]} 100%)`
+        } else {
+            
+            const step = 100 / (colors.length - 1); 
+    
+            const colorStops = colors.map((color, index) => {
+                const position = index * step;
+                return `${color} ${position}%`;
+            });
+    
+            const gradient = `linear-gradient(${angle}deg, ${colorStops.join(', ')})`;
+            return gradient;
         }
-    
-        // Calculate the angle step for the gradient
-        const angleStep = 360 / numColors;
-    
-        // Generate the gradient string
-        const gradientStops = colors.map((color, index) => {
-            const angle = index * angleStep;
-            return `hsl(${angle}deg 100% 50%) ${index * (100 /  numColors - 1)}%`;
-        });
-    
-        const gradientString = `linear-gradient(45deg, ${gradientStops.join(', ')})`;
-        return gradientString;
+        
+
     }
 }
