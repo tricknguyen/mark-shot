6ce2663e33eb68efdaa58b30d86a5e56edc77f2d
anhvunguyen-preciofishbone <vu.nguyen.anh@preciofishbone.se>
2024-05-10 21:45:07 +0700

    [🎃] - Remove inter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+//import { Inter } from "next/font/google";
 import "./globals.css";
 import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
 import icon from "../images/icon.png";
 import Image from "next/image";
 
-const inter = Inter({ subsets: ["latin"] });
+//const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
