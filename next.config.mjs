4519a7e60c1707ea44c696e1bb5c5db01cbdc14b
anhvunguyen-preciofishbone <vu.nguyen.anh@preciofishbone.se>
2024-05-11 15:14:06 +0700

    [🎡] - complete setup config using styleX

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import babelrc from "./.babelrc.js";
+import stylexPlugin from "@stylexjs/nextjs-plugin";
 
-export default nextConfig;
+const plugins = babelrc.plugins;
+const [_name, options] = plugins.find(
+  (plugin) => Array.isArray(plugin) && plugin[0] === '@stylexjs/babel-plugin',
+);
+
+const rootDir = options.unstable_moduleResolution.rootDir ?? __dirname;
+const aliases = options.aliases ?? undefined;
+const useCSSLayers = options.useCSSLayers ?? undefined;
+
+const styleX = stylexPlugin({ rootDir, aliases, useCSSLayers })({
+    transpilePackages: ['@stylexjs/open-props'],
+});
+
+const nextConfig = {
+    experimental: {
+        forceSwcTransforms: true,
+    },
+    styleX
+}
+   
+  export default nextConfig
